dbbb59a8167ac70ec6944686eb27d35430aa7c1c
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-12-03 03:53:03 -0300

    feta: create find user handler factory

--- backend/src/main/factories/handlers/index.ts
+++ backend/src/main/factories/handlers/index.ts
@@ -1,3 +1,4 @@
 export * from './check-user-password-query-handler-factory';
 export * from './create-user-session-query-handler-factory';
 export * from './find-user-by-email-query-handler-factory';
+export * from './find-user-query-handler-factory';
