46e964d9809f9475c44448dcd981d26f793fb918
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-12-03 03:11:40 -0300

    feat: create loader for postgres connection

---
+++ backend/src/main/loaders/index.ts
@@ -0,0 +1,19 @@
+import { PostgresConnection } from '../../infra/database/postgres/connection';
+
+export const loader = async (): Promise<void> => {
+  await Promise.all([
+    PostgresConnection.getInstance().connect({
+      idleInTransactionSessionTimeout: 10000,
+      queryTimeout: 2500,
+      statementTimeout: 2500,
+      connectionTimeout: 2500,
+      database: process.env.POSTGRES_DB,
+      host: process.env.POSTGRES_HOST,
+      idleTimeout: 10000,
+      max: Number(process.env.POSTGRES_MAX),
+      password: process.env.POSTGRES_PASSWORD,
+      port: Number(process.env.POSTGRES_PORT),
+      user: process.env.POSTGRES_USER,
+    }),
+  ]);
+};
