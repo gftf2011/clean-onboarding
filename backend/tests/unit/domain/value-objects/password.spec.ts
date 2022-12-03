d6d0a36b35ffda4d48fb18ad332e4f2f8de1076d
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-12-03 18:42:50 -0300

    test: ensure no encryption password do not accept undefined values

--- backend/tests/unit/domain/value-objects/password.spec.ts
+++ backend/tests/unit/domain/value-objects/password.spec.ts
@@ -3,8 +3,14 @@ import { Password } from '../../../../src/domain/value-objects';
 
 describe('Password', () => {
   describe('No Encryption Password', () => {
-    it('should return "InvalidPasswordError" if email is "null"', () => {
+    it('should return "InvalidPasswordError" if password is "null"', () => {
       const response = Password.create(null as any, false);
+      expect(response.isLeft()).toBeTruthy();
+      expect(response.value).toEqual(new InvalidPasswordError());
+    });
+
+    it('should return "InvalidPasswordError" if password is "undefined"', () => {
+      const response = Password.create(undefined as any, false);
       expect(response.isLeft()).toBeTruthy();
       expect(response.value).toEqual(new InvalidPasswordError());
     });
