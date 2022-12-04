210fd8e52f40c5c3d799b4389b83cc01dcfcc395
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-12-04 11:44:58 -0300

    test: ensure password do not accept value with less than 1 lowercase letter

--- backend/tests/unit/domain/value-objects/password.spec.ts
+++ backend/tests/unit/domain/value-objects/password.spec.ts
@@ -50,5 +50,11 @@ describe('Password', () => {
       expect(response.isLeft()).toBeTruthy();
       expect(response.value).toEqual(new InvalidPasswordError());
     });
+
+    it('should return "InvalidPasswordError" if password has less than 1 lowercase letter', () => {
+      const response = Password.create('12345678AAA', false);
+      expect(response.isLeft()).toBeTruthy();
+      expect(response.value).toEqual(new InvalidPasswordError());
+    });
   });
 });
