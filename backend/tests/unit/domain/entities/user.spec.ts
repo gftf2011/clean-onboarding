09cb05c01c191d9991e6a9fa5505e295144fe9ce
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-12-04 16:41:30 -0300

    test: ensure user entity returns name error with invalid name

--- backend/tests/unit/domain/entities/user.spec.ts
+++ backend/tests/unit/domain/entities/user.spec.ts
@@ -4,7 +4,10 @@ import faker from 'faker';
 
 import { User } from '../../../../src/domain/entities';
 import { Nationalities } from '../../../../src/domain/contracts';
-import { InvalidIdError } from '../../../../src/domain/errors';
+import {
+  InvalidIdError,
+  InvalidNameError,
+} from '../../../../src/domain/errors';
 
 describe('User Entity', () => {
   it('should return "InvalidIdError" if id is invalid', () => {
@@ -25,5 +28,25 @@ describe('User Entity', () => {
     );
     expect(response.isLeft()).toBeTruthy();
     expect(response.value).toEqual(new InvalidIdError(''));
+  });
+
+  it('should return "InvalidNameError" if name is invalid', () => {
+    const response = User.create(
+      '00000000-0000-0000-0000-000000000000',
+      {
+        document: new RandomSSN().value().toString(),
+        email: faker.internet.email().toLowerCase(),
+        lastname: 'test',
+        name: '',
+        password: '12345678aB?',
+        phone: '0000000000',
+      },
+      {
+        encrypted: false,
+        nationality: Nationalities.UNITED_STATES_OF_AMERICA,
+      },
+    );
+    expect(response.isLeft()).toBeTruthy();
+    expect(response.value).toEqual(new InvalidNameError(''));
   });
 });
