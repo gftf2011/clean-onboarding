b17f280801225c8fcda79580154cd5a6c5ffa42d
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-12-06 15:58:04 -0300

    test: ensure sign-up flow returns 400 error with invalid email

--- backend/tests/unit/e2e/sign-up.test.ts
+++ backend/tests/unit/e2e/sign-up.test.ts
@@ -9,6 +9,8 @@ import server from '../../../src/main/config/server';
 
 import { PostgresAdapter } from '../../../src/infra/database/postgres/postgres-adapter';
 
+import { InvalidEmailError } from '../../../src/domain/errors';
+
 import { UserAlreadyExistsError } from '../../../src/application/errors';
 
 describe('Sign-Up Route', () => {
@@ -41,6 +43,34 @@ describe('Sign-Up Route', () => {
       });
 
       expect(response.status).toBe(204);
+    });
+
+    it('should return 400 with invalid email', async () => {
+      const email = '';
+      const password = '12345678xX@';
+      const name = 'test';
+      const lastname = 'test';
+      const locale = 'UNITED_STATES_OF_AMERICA';
+      const phone = faker.phone.phoneNumber('##########');
+      const document = new RandomSSN().value().toString();
+
+      const response = await request(server).post('/api/V1/sign-up').send({
+        email,
+        password,
+        name,
+        lastname,
+        locale,
+        phone,
+        document,
+      });
+
+      const error = new InvalidEmailError('');
+
+      expect(response.status).toBe(400);
+      expect(response.body).toEqual({
+        message: error.message,
+        name: error.name,
+      });
     });
 
     it('should return 403 with a duplicated email', async () => {
