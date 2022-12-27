c78cef003a7d1b53f83af70c29728e3be7d8cb56
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-12-27 15:38:19 -0300

    test: ensure user service returns a user by calling 'find' method

--- backend/tests/unit/application/services/user-service.spec.ts
+++ backend/tests/unit/application/services/user-service.spec.ts
@@ -4,7 +4,10 @@ import { RandomSSN } from 'ssn';
 import { UserModel } from '../../../../src/domain/models';
 
 import { UserService } from '../../../../src/application/services';
-import { FindUserByEmailAction } from '../../../../src/application/actions';
+import {
+  FindUserAction,
+  FindUserByEmailAction,
+} from '../../../../src/application/actions';
 
 import { CommandBusDummy } from '../../../doubles/dummies/bus/commands';
 import { QueryBusStub } from '../../../doubles/stubs/bus/queries';
@@ -45,6 +48,46 @@ describe('User Application Service', () => {
       const sut = new UserService(commandBus, queryBus);
 
       await sut.findByEmail(userEmail);
+
+      expect(queryBus.actions[0].operation).toBe(action.operation);
+      expect(queryBus.actions[0].data).toEqual(action.data);
+    });
+  });
+
+  describe('find - method', () => {
+    it('should return an user as expected result', async () => {
+      const user: UserModel = {
+        id: faker.datatype.uuid(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        name: faker.name.firstName(),
+        lastname: faker.name.lastName(),
+        phone: faker.phone.phoneNumber('##########'),
+        document: new RandomSSN().value().toString(),
+        locale: 'UNITED_STATES_OF_AMERICA',
+      };
+      const queryBus = new QueryBusStub(user);
+      const commandBus = new CommandBusDummy();
+
+      const sut = new UserService(commandBus, queryBus);
+
+      const response = await sut.find(user.id);
+
+      expect(response).toEqual(user);
+    });
+
+    it('should check action', async () => {
+      const userId = faker.datatype.uuid();
+      const action = new FindUserAction({
+        id: userId,
+      });
+
+      const queryBus = new QueryBusSpy();
+      const commandBus = new CommandBusDummy();
+
+      const sut = new UserService(commandBus, queryBus);
+
+      await sut.find(userId);
 
       expect(queryBus.actions[0].operation).toBe(action.operation);
       expect(queryBus.actions[0].data).toEqual(action.data);
