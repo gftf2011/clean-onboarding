b2b62e6ed686f22250468fd1c10e8a3c3ec05016
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-12-28 19:00:51 -0300

    refactor: rewrite messy code

--- backend/tests/unit/application/handlers/find-user-handler.spec.ts
+++ backend/tests/unit/application/handlers/find-user-handler.spec.ts
@@ -5,11 +5,10 @@ import { LocalUserRepositoryFactory } from '../../../../src/infra/repositories';
 
 import { FindUserAction } from '../../../../src/application/actions';
 import { FindUserHandler } from '../../../../src/application/handlers';
-import { UserModel } from '../../../../src/domain/models';
 
 describe('Find User Handler', () => {
-  it('should return user if user exists', async () => {
-    const user: UserModel = {
+  const makeUser = (): any => {
+    return {
       id: faker.datatype.uuid(),
       email: faker.internet.email(),
       password: faker.internet.password(),
@@ -19,23 +18,29 @@ describe('Find User Handler', () => {
       phone: faker.phone.phoneNumber('##########'),
       document: new RandomSSN().value().toString(),
     };
+  };
+
+  it('should return user if user exists', async () => {
+    const user = makeUser();
+
     const repository = new LocalUserRepositoryFactory().createRepository();
 
     await repository.save(user);
 
     const action = new FindUserAction({ id: user.id });
-    const handler = new FindUserHandler(repository);
-    const response = await handler.handle(action);
+    const sut = new FindUserHandler(repository);
+    const response = await sut.handle(action);
 
     expect(response).toEqual(user);
   });
 
   it('should return "undefined" if user does not exists', async () => {
-    const userId = faker.datatype.uuid();
+    const user = makeUser();
+
     const repository = new LocalUserRepositoryFactory().createRepository();
-    const action = new FindUserAction({ id: userId });
-    const handler = new FindUserHandler(repository);
-    const response = await handler.handle(action);
+    const action = new FindUserAction({ id: user.id });
+    const sut = new FindUserHandler(repository);
+    const response = await sut.handle(action);
 
     expect(response).toBeUndefined();
   });
