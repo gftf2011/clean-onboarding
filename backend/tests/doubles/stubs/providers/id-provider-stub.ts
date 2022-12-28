c2f311c67862bd1f100fc8f9cf5be6e88082e20f
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-12-28 12:44:49 -0300

    refactor: rewrite messy code

--- backend/tests/doubles/stubs/providers/id-provider-stub.ts
+++ backend/tests/doubles/stubs/providers/id-provider-stub.ts
@@ -4,8 +4,10 @@ import {
 } from '../../../../src/application/contracts/providers';
 
 export class IDProviderStub implements IDProvider {
+  constructor(private readonly uuidV4?: string) {}
+
   public generateV4(): string {
-    return '00000000-0000-0000-0000-000000000000';
+    return this.uuidV4 || '00000000-0000-0000-0000-000000000000';
   }
 
   public generateV5(name: string, namespace: NAMESPACES): string {
--- backend/tests/unit/application/handlers/create-user-handler.spec.ts
+++ backend/tests/unit/application/handlers/create-user-handler.spec.ts
@@ -3,8 +3,6 @@ import { RandomSSN } from 'ssn';
 
 import { LocalUserRepositoryFactory } from '../../../../src/infra/repositories';
 
-import { Nationalities } from '../../../../src/domain/contracts';
-
 import {
   CreateUserAction,
   SendWelcomeEmailAction,
@@ -19,81 +17,127 @@ import {
 import { InvalidEmailError } from '../../../../src/domain/errors';
 
 describe('Create User Handler', () => {
+  const makeUser = (invalidEmail: boolean): any => {
+    return {
+      id: faker.datatype.uuid(),
+      email: invalidEmail ? '' : faker.internet.email(),
+      password: '12345678aX%',
+      name: faker.name.firstName(),
+      lastname: faker.name.lastName(),
+      phone: faker.phone.phoneNumber('##########'),
+      document: new RandomSSN().value().toString(),
+      locale: 'UNITED_STATES_OF_AMERICA' as any,
+    };
+  };
+
   it('should create user if it not exists first', async () => {
-    const idProvider = new IDProviderStub();
+    const user = makeUser(false);
+
+    const idProvider = new IDProviderStub(user.id);
     const hashProvider = new HashProviderStub();
     const repository = new LocalUserRepositoryFactory().createRepository();
     const publisher = new ActionPublisherSpy();
 
     const action = new CreateUserAction({
-      locale: 'UNITED_STATES_OF_AMERICA' as Nationalities,
+      locale: user.locale,
       user: {
-        document: new RandomSSN().value().toString(),
-        email: faker.internet.email(),
-        lastname: faker.name.lastName(),
-        name: faker.name.firstName(),
-        password: '12345678aX%',
-        phone: faker.phone.phoneNumber('##########'),
+        document: user.document,
+        email: user.email,
+        lastname: user.lastname,
+        name: user.name,
+        password: user.password,
+        phone: user.phone,
       },
     });
-    const handler = new CreateUserHandler(
+    const sut = new CreateUserHandler(
       idProvider,
       hashProvider,
       repository,
       publisher,
     );
 
-    await handler.handle(action);
+    await sut.handle(action);
 
-    const foundUser = await repository.findByEmail(
-      action.data.user.email.toLowerCase(),
-    );
+    const foundUser = await repository.findByEmail(user.email.toLowerCase());
 
-    const sendWelcomeEmailAction = new SendWelcomeEmailAction({
-      fullName: `${action.data.user.name.toLowerCase()} ${action.data.user.lastname.toLowerCase()}`,
-      to: action.data.user.email.toLowerCase(),
-    });
-
-    expect(publisher.actions[0]).toEqual(sendWelcomeEmailAction);
     expect(foundUser).toEqual({
-      id: '00000000-0000-0000-0000-000000000000',
-      email: action.data.user.email.toLowerCase(),
-      lastname: action.data.user.lastname.toLowerCase(),
-      name: action.data.user.name.toLowerCase(),
-      password: `encoded_${
-        action.data.user.password
-      }-${action.data.user.email.toLowerCase()}$${action.data.user.document}`,
-      phone: action.data.user.phone,
-      document: action.data.user.document,
-      locale: 'UNITED_STATES_OF_AMERICA',
+      id: idProvider.generateV4(),
+      email: user.email.toLowerCase(),
+      lastname: user.lastname.toLowerCase(),
+      name: user.name.toLowerCase(),
+      password: await hashProvider.encode(
+        user.password,
+        `${user.email.toLowerCase()}$${user.document}`,
+      ),
+      phone: user.phone,
+      document: user.document,
+      locale: user.locale,
     });
   });
 
-  it('should throw error if an user field is invalid', async () => {
-    const idProvider = new IDProviderStub();
+  it('should check action to send email', async () => {
+    const user = makeUser(false);
+
+    const idProvider = new IDProviderStub(user.id);
     const hashProvider = new HashProviderStub();
     const repository = new LocalUserRepositoryFactory().createRepository();
     const publisher = new ActionPublisherSpy();
 
     const action = new CreateUserAction({
-      locale: 'UNITED_STATES_OF_AMERICA' as Nationalities,
+      locale: user.locale,
       user: {
-        document: new RandomSSN().value().toString(),
-        email: '',
-        lastname: faker.name.lastName(),
-        name: faker.name.firstName(),
-        password: '12345678aX%',
-        phone: faker.phone.phoneNumber('##########'),
+        document: user.document,
+        email: user.email,
+        lastname: user.lastname,
+        name: user.name,
+        password: user.password,
+        phone: user.phone,
       },
     });
-    const handler = new CreateUserHandler(
+    const sut = new CreateUserHandler(
       idProvider,
       hashProvider,
       repository,
       publisher,
     );
 
-    const promise = handler.handle(action);
+    await sut.handle(action);
+
+    const sendWelcomeEmailAction = new SendWelcomeEmailAction({
+      fullName: `${user.name.toLowerCase()} ${user.lastname.toLowerCase()}`,
+      to: user.email.toLowerCase(),
+    });
+
+    expect(publisher.actions[0]).toEqual(sendWelcomeEmailAction);
+  });
+
+  it('should throw error if an user field is invalid', async () => {
+    const user = makeUser(true);
+
+    const idProvider = new IDProviderStub();
+    const hashProvider = new HashProviderStub();
+    const repository = new LocalUserRepositoryFactory().createRepository();
+    const publisher = new ActionPublisherSpy();
+
+    const action = new CreateUserAction({
+      locale: user.locale,
+      user: {
+        document: user.document,
+        email: user.email,
+        lastname: user.lastname,
+        name: user.name,
+        password: user.password,
+        phone: user.phone,
+      },
+    });
+    const sut = new CreateUserHandler(
+      idProvider,
+      hashProvider,
+      repository,
+      publisher,
+    );
+
+    const promise = sut.handle(action);
 
     await expect(promise).rejects.toThrowError(new InvalidEmailError(''));
   });
