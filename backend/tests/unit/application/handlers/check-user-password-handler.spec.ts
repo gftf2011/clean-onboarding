96239ae64f0736358c675a4b0cfc212c853694f1
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-12-28 11:52:10 -0300

    refactor: rewrite messy code

--- backend/tests/unit/application/handlers/check-user-password-handler.spec.ts
+++ backend/tests/unit/application/handlers/check-user-password-handler.spec.ts
@@ -4,20 +4,25 @@ import { RandomSSN } from 'ssn';
 import { CheckUserPasswordAction } from '../../../../src/application/actions';
 import { CheckUserPasswordHandler } from '../../../../src/application/handlers';
 
-import { AccountDTO } from '../../../../src/domain/dtos';
-
 import { HashProviderStub } from '../../../doubles/stubs/providers';
 
 describe('Check User Password', () => {
-  it('should return "true" if password matches with hashed password', async () => {
-    const document = new RandomSSN().value().toString();
-    const account: AccountDTO = {
+  const makeAccount = (): any => {
+    return {
       email: faker.internet.email(),
       password: faker.internet.password(),
     };
+  };
+
+  const makeUserDocument = (): string => {
+    return new RandomSSN().value().toString();
+  };
+
+  it('should return "true" if password matches with hashed password', async () => {
+    const document = makeUserDocument();
+    const account = makeAccount();
 
     const hashProvider = new HashProviderStub();
-
     const hashedPassword = await hashProvider.encode(
       account.password,
       `${account.email}$${document}`,
@@ -28,21 +33,18 @@ describe('Check User Password', () => {
       document,
       hashedPassword,
     });
-    const handler = new CheckUserPasswordHandler(hashProvider);
+    const sut = new CheckUserPasswordHandler(hashProvider);
 
-    const response = await handler.handle(action);
+    const response = await sut.handle(action);
 
     expect(response).toBeTruthy();
   });
 
   it('should return "false" if password does not matches with hashed password', async () => {
-    const document = new RandomSSN().value().toString();
-    const account: AccountDTO = {
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
-    const hashedPassword = 'incorrect_hash_password';
+    const document = makeUserDocument();
+    const account = makeAccount();
 
+    const hashedPassword = 'incorrect_hash_password';
     const hashProvider = new HashProviderStub();
 
     const action = new CheckUserPasswordAction({
@@ -50,9 +52,9 @@ describe('Check User Password', () => {
       document,
       hashedPassword,
     });
-    const handler = new CheckUserPasswordHandler(hashProvider);
+    const sut = new CheckUserPasswordHandler(hashProvider);
 
-    const response = await handler.handle(action);
+    const response = await sut.handle(action);
 
     expect(response).toBeFalsy();
   });
