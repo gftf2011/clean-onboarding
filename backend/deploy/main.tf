817e6bea5ca1ab3e5d1f21bf4a7a3aa5ed37246b
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2023-01-01 06:30:05 -0300

    feat: add aws terraform provider

--- backend/deploy/main.tf
+++ backend/deploy/main.tf
@@ -1,1 +1,13 @@
-terraform {}
+terraform {
+  backend "s3" {
+    bucket = "clean-onboarding-api-devops-tfstate"
+    key = "clean-onboarding-tfstate"
+    region = "us-east-1"
+    encrypt = true
+    dynamodb_table = "clean-onboarding-api-devops-tf-state-lock"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
